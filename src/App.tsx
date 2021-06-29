9626b25c21403130d6ea4596c7739acf89cb1848
Алекандр Авдеев <alexander.avdeev@balance-pl.ru>
2021-06-29 08:48:51 +0300

    Добавил алиасы по "@" как root директории проекта.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import React, { memo } from 'react';
+import React, { memo } from "react";
 
-import styles from './style.module.scss';
+import styles from "./style.module.scss";
 
-import background from './example-img.jpg';
+import background from "./example-img.jpg";
+import Typography from "@/components/Typography";
 
-import Typography from '@components/Typography';
-
-const App = () => {
-    return (
-        <div className={styles.App}>
-            <h1 className="red">Beginning my react create App</h1>
-            <img src={background} alt="horses" />
-            <Typography>Helloo))</Typography>
-        </div>
-    );
+const App = (props: any) => {
+  return (
+    <div className={styles.App}>
+      <h1 className="red">Beginning my react create App</h1>
+      <img src={background} alt="horses" />
+      <Typography>Helloo))</Typography>
+    </div>
+  );
 };
 
 export default memo(App);
--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,9 +1,7 @@
-import React, {memo, ReactNode} from 'react';
+import React, { memo, ReactNode } from "react";
 
-const Typography = ({children}: {
-    children: ReactNode
-}) => {
-    return <h1>{children}</h1>
+const Typography = ({ children }: { children: ReactNode }) => {
+  return <h1>{children}</h1>;
 };
 
 export default memo(Typography);
